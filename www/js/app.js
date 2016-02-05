27eed2a788ba5c65bc5e0229834e31c37d1e9e4a
raul-ayi <raul.nunez@ayi-asociados.com>
2016-02-05 15:58:11 -0300

    adding modules to the app

--- www/js/app.js
+++ www/js/app.js
@@ -1,33 +1,59 @@
-// Ionic Starter App
+/**************************************************
++	Module:		ionicApp
+**************************************************/
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('ionicApp', ['ionic'])
+angular.module('ionicApp', ['ionic','ionicApp.Main','ionicApp.Page2'])
+
+.config(function($urlRouterProvider) {
+  // Default route for the whole app
+  $urlRouterProvider.otherwise('/main');
+});
 
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+/**************************************************
++	Module:		ionicApp.Main
++	Exports:
++		MainCtrl
+**************************************************/
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+angular.module('ionicApp.Main', ['ionic'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('main', {
+    url: '/main',
+    templateUrl: 'templates/main.html',
+    controller: 'MainCtrl'
   });
+})
+
+.controller("MainCtrl", function() {
+  console.log("Main Controller says: Hello World");
+});
+
+
+/**************************************************
++	Module:		ionicApp.Page2
++	Exports:
++		userService
++		Page2Ctrl
+**************************************************/
+
+angular.module('ionicApp.Page2', ['ionic'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('page2', {
+    url: '/page2',
+    templateUrl: 'templates/page2.html',
+    controller: 'Page2Ctrl'
+  })
 })
 
 .factory('userService', function($http) {
   return {
     getUsers: function() {
-      return $http.get('https://randomuser.me/api/?results=10').then(function(response) {
+      return $http.get('http://randomuser.me/api/?results=10').then (function(response) {
         return response.data.results;
       });
     }
@@ -35,36 +61,7 @@ angular.module('ionicApp', ['ionic'])
 })
 
 .controller("Page2Ctrl", function($scope, userService) {
-  userService.getUsers().then(function(users) {
+  userService.getUsers().then(function(users){
     $scope.users = users;
   });
-
-})
-
-.controller("MainCtrl", function() {
-  console.log("Main Controller says: Hello World!");
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-  .state('main', {
-    url: '/main',
-    templateUrl: 'templates/main.html',
-    controller: 'MainCtrl'
-  })
-
-  .state('page2', {
-    url: '/page2',
-    templateUrl: 'templates/page2.html',
-    controller: 'Page2Ctrl'
-  })
-
-  .state('page3', {
-    url: '/page3',
-    templateUrl: 'templates/page3.html'
-  })
-// if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/main');
-
 });
